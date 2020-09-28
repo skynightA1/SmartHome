ff23dba79bc50f6e86d10d93e6ac15c41e039da6
skynightA1 <epic000319@gmail.com>
2020-09-28 15:36:57 +0800

    增加蜂鳴器功能

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -6,9 +6,11 @@ pinMode(11, OUTPUT);
 void loop() 
 { 
 digitalWrite(12, HIGH); 
-digitalWrite(11, LOW); 
+digitalWrite(11, LOW);
+tone(7, 1000, 500); 
 delay(500); 
 digitalWrite(12, LOW); 
 digitalWrite(11, HIGH); 
+tone(7, 800, 500);
 delay(500); 
 }
