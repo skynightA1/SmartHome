0c8eed1de4ff3e92817149ae3e1e16c94b587192
skynightA1 <epic000319@gmail.com>
2020-09-28 16:18:58 +0800

    修改為斷線時才發警報

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,17 +1,21 @@
-void setup() 
-{ 
-pinMode(12, OUTPUT); 
+void setup()
+{
+pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
 pinMode(7, OUTPUT);
-} 
-void loop() 
-{ 
-digitalWrite(12, HIGH); 
+pinMode(2, INPUT);
+Serial.begin(9600);
+}
+void loop()
+{
+if(digitalRead(2)==HIGH) {
+digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
-tone(7, 1000, 500); 
-delay(500); 
-digitalWrite(12, LOW); 
-digitalWrite(11, HIGH); 
+tone(7, 1000, 500);
+delay(500);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
 tone(7, 800, 500);
-delay(500); 
+delay(500);
+}
 }
